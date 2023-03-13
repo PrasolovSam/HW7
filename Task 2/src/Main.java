a28c9dc3e81db7be5cb3e9ea0fa27c67ab83af00
Roman Prasolov <rvprasolov@me.com>
2023-03-13 18:42:37 +0400

    Задача решена

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        //todo Тут увольняем айтишников
+        /*
+        for(int i = 0; i < 100; i = i + 2){
+            System.out.println("Уволен айтишник с id " + i);
+        }
+         */
+        int i = 0;
+        while(i < 100){
+            System.out.println("Уволен айтишник с id " + i);
+            i = i + 2;
+        }
     }
 }
